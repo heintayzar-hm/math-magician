40577b2239a7766426b0c24856c433d0afbefa95
Hein Tay Zar <heintayzarhm@gmail.com>
2022-11-29 19:21:03 +0630

    refactor to hooks

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,33 +1,31 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import { decodeHTMLEntities } from '../helper/util';
 import calculate from '../logic/calculate';
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick = (e) => {
-    const calculateObj = { ...this.state };
+function Calculator() {
+  const calculatorObj = {
+    total: 0,
+    next: null,
+    operation: null,
+  };
+
+  const [getCalculatorObj, setCalculatorObj] = useState(calculatorObj);
+
+  function handleClick(e) {
+    const calculateObj = { ...getCalculatorObj };
     const calculatedObj = calculate(calculateObj, e.target.innerText);
-    this.setState(calculatedObj);
+    setCalculatorObj(calculatedObj);
   }
 
-  render() {
-    const calculatorEl = [['AC', '+/-', '%', '÷'], [7, 8, 9, 'x'], [4, 5, 6, '-'], [1, 2, 3, '+'], [0, '.', '=']];
-    const { total, next, operation } = this.state;
-    return (
-      <>
-        <section id="calculator" className="lg:w-1/2 w-full">
-          <div className="w-full leading-[80px] bg-zinc-500 text-white text-right text-3xl">
-            {
-            (total === null && operation === null && next === null) ? 0
+  const calculatorEl = [['AC', '+/-', '%', '÷'], [7, 8, 9, 'x'], [4, 5, 6, '-'], [1, 2, 3, '+'], [0, '.', '=']];
+  const { total, next, operation } = getCalculatorObj;
+  return (
+    <>
+      <section id="calculator" className="lg:w-1/2 w-full">
+        <div className="w-full leading-[80px] bg-zinc-500 text-white text-right text-3xl">
+          {
+            (!total && !operation && !next) ? 0
               : (
                 <>
                   { total }
@@ -37,31 +35,30 @@ class Calculator extends React.Component {
               )
             }
 
-          </div>
-          <div className="grid grid-cols-4  leading-[80px] text-center bg-slate-200">
-            {calculatorEl.map((item) => (
-              (item.length === 4) ? (
-                <>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[0]}</button>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[1]}</button>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[2]}</button>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer bg-yellow-600 border-[1px] border-double border-gray-400">{decodeHTMLEntities(item[3])}</button>
-                </>
-              ) : (
-                <>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer col-span-2 text-center border-[1px] border-double border-gray-400">{item[0]}</button>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[1]}</button>
-                  <button type="button" onClick={this.handleClick} className="cursor-pointer bg-yellow-600 border-[1px] border-double border-gray-400">{item[2]}</button>
-                </>
-              )
+        </div>
+        <div className="grid grid-cols-4  leading-[80px] text-center bg-slate-200">
+          {calculatorEl.map((item) => (
+            (item.length === 4) ? (
+              <>
+                <button type="button" onClick={handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[0]}</button>
+                <button type="button" onClick={handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[1]}</button>
+                <button type="button" onClick={handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[2]}</button>
+                <button type="button" onClick={handleClick} className="cursor-pointer bg-yellow-600 border-[1px] border-double border-gray-400">{decodeHTMLEntities(item[3])}</button>
+              </>
+            ) : (
+              <>
+                <button type="button" onClick={handleClick} className="cursor-pointer col-span-2 text-center border-[1px] border-double border-gray-400">{item[0]}</button>
+                <button type="button" onClick={handleClick} className="cursor-pointer border-[1px] border-double border-gray-400">{item[1]}</button>
+                <button type="button" onClick={handleClick} className="cursor-pointer bg-yellow-600 border-[1px] border-double border-gray-400">{item[2]}</button>
+              </>
+            )
 
-            ))}
+          ))}
 
-          </div>
-        </section>
-      </>
-    );
-  }
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Calculator;
